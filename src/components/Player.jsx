1c986cd2bacdcac075802e958959f206f002dde0
Balakrishna kolla <balakrishnakolla00014@gmail.com>
2024-05-20 22:11:05 +0530

    feature/ previous-next functionality click songs play functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { PlayerContext } from '../context/PlayerContext'
 const Player = () => {
-    const { seekbar, seekbg, playStatus, play, pause ,Track,Time} = useContext(PlayerContext);
+    const { seekbar, seekbg, playStatus, play, pause ,Track,Time,previous,next} = useContext(PlayerContext);
     return (
         <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
             <div className="hidden lg:flex items-center gap-4">
@@ -15,18 +15,18 @@ const Player = () => {
             <div className="flex flex-col items-center gap-1  m-auto">
                 <div className="flex gap-4 ">
                     <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt="" />
-                    <img className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
+                    <img onClick={previous} className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
                     {playStatus
                         ? <img onClick={pause} className='w-4 cursor-pointer' src={assets.pause_icon} alt="" />
                         : <img onClick={play} className='w-4 cursor-pointer' src={assets.play_icon} alt="" />
                     }
-                    <img className='w-4 cursor-pointer' src={assets.next_icon} alt="" />
+                    <img onClick={next} className='w-4 cursor-pointer' src={assets.next_icon} alt="" />
                     <img className='w-4 cursor-pointer' src={assets.loop_icon} alt="" />
                 </div>
                 <div className="flex items-center gap-5">
                     <p>{Time.currentTime.minute}: {Time.currentTime.second}</p>
                     <div ref={seekbg} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
-                        <hr ref={seekbar} className='h-1 border-none w-10 bg-green-800 rounded-full ' />
+                        <hr ref={seekbar} className='h-1 border-none w-0 bg-green-800 rounded-full ' />
                     </div>
                     <p>{Time.totalTime.minute}: {Time.totalTime.second}</p>
                 </div>
--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PlayerContext } from '../context/PlayerContext'
 
 const AlbumItem = ({image,name,desc,id}) => {
+  const {playwithid} = useContext(PlayerContext);
   return (
-    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+    <div onClick={()=>playwithid(id)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
         <img className='rounded' src={image} alt="" />
         <p className='font-bold mt-2 mb-1 '>{name}</p>
         <p className='text-slate-200 text-sm'>{desc}</p>
